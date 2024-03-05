388223e9055a8e293b28b736a606c6b7b216d385
Jaime Jonathan Ortiz Velazco <jonathan.ortiz.velazco@gmail.com>
2024-03-05 12:36:44 -0600

    Adding cases where remaining values is less than 4, greater than 4, equals to 9 and equals to zero. Also adding test cases for different scenarios

--- src/Main.java
+++ src/Main.java
@@ -5,58 +5,91 @@ public class Main {
     // Method Number to string roman
     // from 1 to 3000
     public static void main(String[] args) {
-        convertToRoman(544);
-//        convertToRoman(1354);
-//        convertToRoman(8);
-//        convertToRoman(56);
-        System.out.println("Hello world!");
+        // Cases where 5 is used
+        convertToRoman(545);
+        // Cases where 4 is used
+        convertToRoman(440);
+        convertToRoman(123);
+        convertToRoman(1224);
+        // Cases where some values are less or greater than 5, excluding 4
+        convertToRoman(3);
+        convertToRoman(27);
+        convertToRoman(13);
+        convertToRoman(362);
+        convertToRoman(8);
+        // Cases using 9s
+        convertToRoman(9);
+        convertToRoman(19);
+        convertToRoman(49);
+        convertToRoman(99);
+        // Invalid case
+        convertToRoman(3001);
+        // Case where mas value is 3000
+        convertToRoman(3000);
+
     }
 
     public static void convertToRoman(int value) {
+        if (value > 3000) {
+            System.out.println("This operation is not allowed: " + value);
+            return;
+        }
+        Map<Integer, Character> romanDigits = new HashMap<>();
+        romanDigits.put(1, 'I');
+        romanDigits.put(5, 'V');
+        romanDigits.put(10, 'X');
+        romanDigits.put(50, 'L');
+        romanDigits.put(100, 'C');
+        romanDigits.put(500, 'D');
+        romanDigits.put(1000, 'M');
 
-        //        String [] romanDigits = {"I","V","X", "L","C","D", "M"};
-        Map<Integer,Character> romanDigits = new HashMap<>();
-        romanDigits.put(1,'I');
-        romanDigits.put(5,'V');
-        romanDigits.put(10,'X');
-        romanDigits.put(50,'L');
-        romanDigits.put(100,'C');
-        romanDigits.put(500,'D');
-        romanDigits.put(1000,'M');
-
-        // 254 CCLIV
-
-        // 254 % 100 = 54 -> 254 / 100 = 2  CC
-        // 54 % 10 = 4 -> 54 / 10 = 5; L
-        // 4 % 1 = 4  / 1 = 4 IV
+        int originalValue = value;
         int maxDivider = 1;
-        int dividerIndex = -1;
 
         while (value / maxDivider >= 1) {
-            dividerIndex ++;
             maxDivider *= 10;
         }
         maxDivider /= 10;
 
-        // 544
         StringBuilder romanNumberString = new StringBuilder();
-        for (int i = maxDivider; i > 0; i = i/10) {
+        for (int i = maxDivider; i > 0; i = i / 10) {
             int divisionResult = value / i;
+            // Case where division is zero, nos printing a value
+            if (divisionResult == 0) {
+                continue;
+            }
             value = value % i;
-            // case value = 5 V
-            if (divisionResult == 5){
-                romanNumberString.append(romanDigits.get(divisionResult * i));
-            }
-            if(divisionResult == 4){
+            // case value = 4 Example : IV , XL, CD
+            if (divisionResult == 4) {
                 romanNumberString.append(romanDigits.get(i));
-                romanNumberString.append(romanDigits.get( (divisionResult + 1) * i));
+                romanNumberString.append(romanDigits.get((divisionResult + 1) * i));
+                continue;
             }
-            // case value = 4 IV
-            // case value < 4 III
-            // case value > 5 VIII
-            // case value 9 IX
-//            if(value > 5)
+            // case value = 5 Example : V , L , D
+            if (divisionResult == 5) {
+                romanNumberString.append(romanDigits.get(divisionResult * i));
+                continue;
+            }
+            // case value < 4 Example : III
+            if (divisionResult < 4) {
+                for (int j = 0; j < divisionResult; j++) {
+                    romanNumberString.append(romanDigits.get(i));
+                }
+                continue;
+            }
+            // case value > 5 Example : VIII
+            if (divisionResult < 9) {
+                romanNumberString.append(romanDigits.get(5 * i));
+                for (int j = 0; j < divisionResult - 5; j++) {
+                    romanNumberString.append(romanDigits.get(i));
+                }
+            }
+            // case value 9 Example: IX
+            if (divisionResult == 9) {
+                romanNumberString.append(romanDigits.get(i));
+                romanNumberString.append(romanDigits.get((i * 10)));
+            }
         }
-        System.out.println(romanNumberString.toString());
+        System.out.println(originalValue + " converted to roman is: " + romanNumberString);
     }
 }
